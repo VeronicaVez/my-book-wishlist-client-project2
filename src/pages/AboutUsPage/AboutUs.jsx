bc74a2913b950fbacad586908eb4901ad9a71ecb
Veronica Vezzoli <veronicavezzoli94@gmail.com>
2024-02-29 17:06:43 +0100

    car responsive

--- src/pages/AboutUsPage/AboutUs.jsx
+++ src/pages/AboutUsPage/AboutUs.jsx
@@ -15,33 +15,23 @@ export default function AboutUs() {
             </h1>
 
             <Row className="mt-5 justify-content-center">
-
-                <Col md={6} className="fran">
-
+                <Col xs={12} md={6} className="photo">
                     <Card style={{ width: '25rem' }} className="card">
-
                         <Card.Img variant="top" src={fran} />
-
                         <Card.Body>
-
                             <Card.Title className="title">
                                 Francisco Pazos
                             </Card.Title>
-
                             <Card.Text>
                                 A passionate front-end developer currently immersed in a web development bootcamp at Ironhack Spain. Thrives on challenging projects that push the boundaries of creativity and innovation. Committed to mastering the art of creating seamless and visually stunning user experiences without forgetting to have fun in the process.
                             </Card.Text>
-
                         </Card.Body>
-
                         <ListGroup className="list-group-flush">
-
                             <Card.Link
                                 href="https://linkedin.com/in/francisco-pazos-rey-bb37bb86"
                                 target="_blank"
                                 className="link">
                                 <ListGroup.Item>LinkedIn</ListGroup.Item></Card.Link>
-
                             <Card.Link
                                 href="https://github.com/franpazos"
                                 target="_blank"
@@ -49,26 +39,18 @@ export default function AboutUs() {
                                 <ListGroup.Item>GitHub</ListGroup.Item></Card.Link>
 
                         </ListGroup>
-
                     </Card>
-
                 </Col>
-
-                <Col md={6}>
-
+                <Col xs={12} md={6} className="photo">
                     <Card style={{ width: '30rem' }} className="card">
-
                         <Card.Img variant="top" src={vero} />
-
                         <Card.Body>
                             <Card.Title>Veronica Vezzoli</Card.Title>
                             <Card.Text>
                                 An inspiring front-end developer at Ironhack Spain, navigating the immersive world of web development with passion and determination. Fueled by kindness, intelligence, and a keen eye for design, she effortlessly overcomes challenges, transforming lines of code into visually stunning and user-friendly digital experiences. In this journey of growth, her creativity shines, making her a standout in the evolving landscape of web development.
                             </Card.Text>
                         </Card.Body>
-
                         <ListGroup className="list-group-flush">
-
                             <Card.Link
                                 href="https://www.linkedin.com/in/veronica-vezzoli/"
                                 target="_blank"
